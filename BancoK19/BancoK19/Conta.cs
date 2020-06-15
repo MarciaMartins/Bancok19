be7b74787e60f9ff2cfbbee977f3134ae00285f0
Marcia Martins <marciapmartins@gmail.com>
2020-06-15 16:18:28 +0100

    Criaçao da classe Conta

---
+++ BancoK19/BancoK19/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoK19
+{
+    public class Conta
+    {
+        public double saldo;
+        public double limite=500;
+        public int numero;
+    }
+}
--- BancoK19/BancoK19/Program.cs
+++ BancoK19/BancoK19/Program.cs
@@ -10,6 +10,20 @@ namespace BancoK19
     {
         static void Main(string[] args)
         {
+            Conta conta01 = new Conta();
+            conta01.saldo = 1000.0;
+            //conta01.limite = 500.0;
+            conta01.numero = 1;
+
+            Console.WriteLine($"- Dados da Conta");
+            Console.WriteLine($"Saldo: {conta01.saldo}");
+            Console.WriteLine($"Limite: {conta01.limite}");
+            Console.WriteLine($"Numero: {conta01.numero}");
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
